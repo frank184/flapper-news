0a69ebb8be3dc91efebf3f4d8ab8aaa74f8e0c90
Francois Belanger <frank@smartbuildingapps.com>
2016-08-07 14:24:15 -0400

    added users to posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, only: [:create, :upvote]
 
   def index
     respond_with Post.all.includes(:comments)
   end
 
   def create
-    respond_with Post.create(permitted_post_params)
+    respond_with Post.create(permitted_post_params.merge(user_id: current_user.id))
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'pp'
-
+pp user = User.create_with(password: 'password').find_or_create_by(email: 'admin@mail.com', username: 'admin')
 posts = [
   { title: 'Google', link: 'https://www.google.com'},
   { title: 'Facebook', link: 'https://www.facebook.com'},
   { title: 'StackOverflow', link: 'https://www.stackoverflow.com'},
   { title: 'Github', link: 'https://www.github.com'},
   { title: 'Reddit', link: 'https://www.reddit.com'}
-].each { |post_attributes| pp Post.find_or_create_by(post_attributes) }
+].each { |post_attributes| pp Post.find_or_create_by(post_attributes.merge(user: user)) }
