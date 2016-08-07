03ac5aafe8ff70b91e24450fccb16ea4b7893d24
Francois Belanger <frank@smartbuildingapps.com>
2016-08-07 13:34:37 -0400

    added angular AuthCtrl.login and AuthCtrl.register, fixed NavCtrl and updated _nav.html

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -5,17 +5,22 @@ angular.module('flapperNews')
   function($scope, Auth) {
     $scope.signedIn = Auth.isAuthenticated
     $scope.logout = Auth.logout
-    Auth.currentUser().then(function (user) {
-      $scope.user = user
-    })
-    $scope.$on('devise:new-registration', function(e, user) {
-      $scope.user = user
-    })
-    $scope.$on('devise:login', function(e, user) {
-      $scope.user = user
-    })
-    $scope.$on('devise:logout', function(e, user) {
-      $scope.user = {}
-    })
+    Auth
+      .currentUser()
+      .then(function (user) {
+        $scope.user = user
+      })
+
+    $scope.$on('devise:new-registration', function (e, user){
+      $scope.user = user;
+    });
+
+    $scope.$on('devise:login', function (e, user){
+      $scope.user = user;
+    });
+
+    $scope.$on('devise:logout', function (e, user){
+      $scope.user = {};
+    });
   }
 ])
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   respond_to :json
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  after_filter :set_csrf_cookie_for_ng
 
   def angular
     render 'layouts/application'
   end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
+
+  private
+    def set_csrf_cookie_for_ng
+      cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+    end
 end
